3657fd4e5ce11fe22defa2fc024732e25eaee19f
Andy Wood <andy@groupzap.com>
2016-02-28 17:13:42 +0000

    Attempt to use cache control to help with polling on the client side, however doesn't work.

--- app/app.py
+++ app/app.py
@@ -104,6 +104,11 @@ def rules():
             print "rule: %s" % title
     return jsonify(result='ok')
 
+@app.after_request
+def add_header(response):
+    response.cache_control.max_age = 60
+    return response
+
 if __name__ == '__main__':
     if sys.argv[1] == 'ev3':
         import ev
