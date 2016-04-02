60f253b39b31ea43f58dc2c0846e0a23fe3e1036
Andy Wood <andy@groupzap.com>
2016-04-02 11:11:42 +0100

    Failed attempt to use gevent to run HTTP and HTTPS at the same time.
    Think it would've worked, but there seems to be a bug in the gevent SSLContext setup.

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, abort, render_template, jsonify, request
-from flask_socketio import SocketIO
 import sys
 from time import sleep
+from flask import Flask, abort, render_template, jsonify, request
+from flask_socketio import SocketIO
+import gevent.monkey
+gevent.monkey.patch_all()
+from gevent.pywsgi import WSGIServer
+from ssl import SSLContext
 
 from sense import SensorThread
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'webrover1'
@@ -17,18 +22,6 @@ def page_not_found(error):
 def index():
     return render_template('index.html', title='PiStorms Mars Rover')
 
-@app.route('/camera/')
-def camera():
-    return render_template('camera.html')
-
-@app.route('/upload/', methods=['POST'])
-def upload():
-    file = open('/home/robot/webrover1/app/static/images/camera.jpg', 'wb')
-    file.write(request.get_data())
-    file.close()
-    socketio.emit('refresh')
-    return 'success'
-
 @app.route('/forward/')
 def forward():
     '''
@@ -39,7 +32,6 @@ def forward():
         return 'success'
     except:
         abort(404)
-
 
 @app.route('/stop/')
 def stop():
@@ -114,6 +106,21 @@ def rules():
             print "rule: %s" % title
     return jsonify(result='ok')
 
+
+cam_app = Flask(__name__)
+@cam_app.route('/camera/')
+def camera():
+    return render_template('camera.html')
+
+@cam_app.route('/upload/', methods=['POST'])
+def upload():
+    file = open('/home/robot/webrover1/app/static/images/camera.jpg', 'wb')
+    file.write(request.get_data())
+    file.close()
+    socketio.emit('refresh')
+    return 'success'
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'nxt2':
         import nxt2
@@ -121,5 +128,7 @@ if __name__ == '__main__':
     else:
         import ev3
         robot = ev3
-    thread = SensorThread(robot, socketio)
-    socketio.run(app, host='0.0.0.0', port=5443, keyfile='legorover.key', certfile='legorover.crt', debug=True)
+    #thread = SensorThread(robot, socketio)
+    WSGIServer(('', 5443), cam_app, keyfile='legorover.key', certfile='legorover.crt').start()
+    print 'running socketio'
+    socketio.run(app, host='0.0.0.0', port=5080) #443, keyfile='legorover.key', certfile='legorover.crt', debug=True)
